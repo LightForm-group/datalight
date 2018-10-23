9d8a6118f5d5a9e9cbbcc0961d4f564b3be2d98b
Nicolas Gruel <nicolas.gruel@manchester.ac.uk>
2018-10-23 14:50:02 +0200

    modify name for metadata description

--- datalight/schemas/zenodo/yamlschema.py
+++ datalight/schemas/zenodo/yamlschema.py
@@ -2,11 +2,11 @@ import sys
 import yaml
 import jsonschema
 
-with open('metadata-1.0.0.yml') as f:
+with open('record-1.0.0.yml') as f:
     schema = yaml.load(f)
 
 #with open('lightform.yml') as f:
-#with open('minimum.yaml') as f: 
+#with open('minimum.yaml') as f:
 with open(sys.argv[1]) as f:
     test = yaml.load(f)
 
