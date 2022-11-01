cb734723db15290232b978da02e919c4b5b575bf
APlowman <adam.jp@live.co.uk>
2022-11-01 13:54:10 +0000

    fix: use find_packages

--- setup.py
+++ setup.py
@@ -1,24 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-requirements = [
-    'requests',
-    'PyYaml>=5.1',
-    'jsonschema',
-    'PyQt5'
-]
+requirements = ["requests", "PyYaml>=5.1", "jsonschema", "PyQt5"]
 
-setup(name='datalight',
-      packages=['datalight'],
-      version="0.9.1",
-      description='Data uploader to Zenodo repository',
-      long_description=readme,
-      author='Peter Crowther',
-      author_email='peter.crowther-3@manchester.ac.uk',
-      url='https://github.com/LightForm-group/datalight',
-      keywords=[],
-      install_requires=requirements,
-      license='MIT',
-      )
+setup(
+    name="datalight",
+    packages=find_packages(),
+    version="0.9.1",
+    description="Data uploader to Zenodo repository",
+    long_description=readme,
+    author="Peter Crowther",
+    author_email="peter.crowther-3@manchester.ac.uk",
+    maintainer="Adam Plowman",
+    maintainer_email="adam.plowman@manchester.ac.uk",
+    url="https://github.com/LightForm-group/datalight",
+    keywords=[],
+    install_requires=requirements,
+    license="MIT",
+)
